fa6e7ad6a69cda279c79c614c216dc7b042b325f
Leonardo Javier Andrade <leonardo.j.andrade88@gmail.com>
2026-07-12 23:31:49 -0300

    agregamos el validation zod a las routes

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,9 +2,19 @@ import { Router } from "express"
 import { limiter } from "../middlewares/limiterMiddleware.js"
 import { register, login } from "../controllers/authControllers.js"
 
+
+/// aqui importamos le middleware de validacion y los schemas de validacion
+import { validateBody } from "../middlewares/validateMiddleware.js"
+import { registerSchema, loginSchema } from "../validation/authValidation.js"
+
+
+// aplicamos el middleware de validacion a las rutas de registro y login, para que antes de llegar al controlador, 
+// se valide el body de la request segun el schema correspondiente
+
+
 const AuthRouter = Router()
 
-AuthRouter.post("/register", register)
-AuthRouter.post("/login", limiter, login)
+AuthRouter.post("/register", validateBody(registerSchema), register)
+AuthRouter.post("/login", validateBody(loginSchema), limiter, login)
 
 export { AuthRouter }
