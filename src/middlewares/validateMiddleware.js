69dfb7514c0353a8fc1c63cb91a07e1b25a071b9
Leonardo Javier Andrade <leonardo.j.andrade88@gmail.com>
2026-07-13 16:01:24 -0300

    correcciones de mensaje res.json validationZod

--- src/middlewares/validateMiddleware.js
+++ src/middlewares/validateMiddleware.js
@@ -1,24 +1,21 @@
+import { ZodError } from "zod";
+
 const validateBody = (schema) => (req, res, next) => {
   try {
     schema.parse(req.body);
     next();
   } catch (error) {
-    // 1. Mapeamos la lista completa de errores por si los necesitas después
-    const errorDetails = error.errors.map((err) => ({
-      campo: err.path[0],
-      mensaje: err.message
-    }));
+    // Si el error es de Zod, tomamos el mensaje del primer fallo
+    console.log("Aqui sucedio un error en el middleware validation")
+  console.log(error.issues[0].message  )  
 
-    // 2. Tomamos el mensaje del primer error de la lista
-    const primerMensaje = errorDetails[0].mensaje;
+      return res.status(400).json({
+        success: false,
+        message: error.message
+      });
+    }
 
-    // 3. Devolvemos el formato exacto de tu imagen adaptado a un error 400
-    return res.status(400).json({
-      success: false,
-      data: null,
-      message: primerMensaje // <-- Aquí viaja "El usuario debe tener al menos 3 caracteres"
-    });
   }
-};
+;
 
 export { validateBody };
